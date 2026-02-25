38e01820239c716f901ac93566b3002d30b241a9
abdelrhman <abdoabdelhay110@gmail.com>
2026-02-25 15:30:17 +0200

    fix

--- app/src/lib/axios.ts
+++ app/src/lib/axios.ts
@@ -1,6 +1,7 @@
 import { API_URL } from "@/constants/constants";
 import axios from "axios";
 import { parseCookies } from "nookies";
+import { useUserStore } from "@/stores/useUserStore";
 
 export const axiosInstance = axios.create({
   baseURL: `${API_URL}`,
@@ -10,8 +11,7 @@ export const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const cookies = parseCookies();
-  const token = cookies.token;
+  const token = useUserStore.getState().token;
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
