5ccf01632942b66d5995dc3dec30508164a724f3
abdelrhman <abdoabdelhay110@gmail.com>
2026-02-25 16:22:51 +0200

    fix

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,25 +1,26 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import jwt from "jsonwebtoken"
 
 export function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname
-    // Only run middleware for dashboard routes
-    if (!path.startsWith('/dashboard')) {
-        return NextResponse.next()
-    }
 
-    // Check for auth token and user credentials in cookies
-    const token = request.cookies.get('token')
-    const userCred = JSON.parse(request.cookies.get('CRED')?.value || '{}')
+const tokenValue = request.cookies.get("token")?.value
 
-    const authorized = token && userCred?.role === "admin"
-    console.log({ authorized, token, userCred })
-    // If not authorized, redirect to signIn
-    if (!authorized) {
-        return NextResponse.redirect(new URL('/signIn', request.url))
-    }
+if (!tokenValue) {
+  return NextResponse.redirect(new URL('/signIn', request.url))
+}
 
-    return NextResponse.next()
+try {
+  const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET!) as any
+
+  if (decoded.role !== "admin") {
+    return NextResponse.redirect(new URL('/signIn', request.url))
+  }
+
+  return NextResponse.next()
+} catch (err) {
+  return NextResponse.redirect(new URL('/signIn', request.url))
+}
 }
 
 export const config = {
